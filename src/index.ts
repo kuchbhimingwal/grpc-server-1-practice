5f4f3b582a3e836843b7b54501ce4f05ae5dac69
kuchbhimingwal <shubhammingi@gmail.com>
2024-07-06 21:48:16 -0400

    grpc server practice inint !!!!

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import path from 'path';
+import * as grpc from '@grpc/grpc-js';
+import  { GrpcObject, ServiceClientConstructor } from "@grpc/grpc-js"
+import * as protoLoader from '@grpc/proto-loader';
+
+const packageDefinition = protoLoader.loadSync(path.join(__dirname, './a.proto'));
+
+const personProto = grpc.loadPackageDefinition(packageDefinition);
+
+const PERSONS = [
+    {
+        name: "harkirat",
+        age: 45
+    },
+    {
+      name: "raman",
+      age: 45
+    },
+];
+
+//@ts-ignore
+// call => req
+// calback => res.json()
+function addPerson(call, callback) {
+  console.log(call)
+    let person = {
+      name: call.request.name,
+      age: call.request.age
+    }
+    PERSONS.push(person);
+    callback(null, person)
+}
+
+//const app = express()
+const server = new grpc.Server();
+
+// app.use("/person", routeHandler)
+server.addService((personProto.AddressBookService as ServiceClientConstructor).service, { addPerson: addPerson });
+server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
+    server.start();
+});
